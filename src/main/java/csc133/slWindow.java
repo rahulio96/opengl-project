cdbda204ed855b17d424a42d2088b5e48d1bf5cf
rahulio96 <rahulgithub96@gmail.com>
2024-02-09 20:49:23 -0800

    Initial commit

---
+++ src/main/java/csc133/slWindow.java
@@ -0,0 +1,10 @@
+
+package csc133;
+
+public class slWindow {
+
+    void slWindow(int win_width, int win_height) {
+        System.out.println("Call to slWindow:: (width, height) == ("
+                        + win_width + ", " + win_height +") received!");
+    }
+}
