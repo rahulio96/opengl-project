cf0ecad1e085228c61af8ca34d922309f1b1eec4
rahulio96 <rahulgithub96@gmail.com>
2024-03-04 13:14:30 -0800

    Changed offset, padding, etc.

--- src/main/java/csc133/spot.java
+++ src/main/java/csc133/spot.java
@@ -11,7 +11,7 @@ public class spot {
     public static int WIN_WIDTH = 900, WIN_HEIGHT = 900;
     public static int vps = 4, fpv = 2, ips = 6; // vertices per square, float per vertices, indices per square
     public static int MAX_ROWS = 20, MAX_COLS = 18; // rows and cols for the square matrix
-    public static int offset = 10, length = 10, padding = 5;
+    public static int offset = 20, length = 30, padding = 10;
     public static float red = 0.0f, green = 0.0f, blue = 1.0f, alpha = 1.0f; // background color
     public static final Vector3f VEC_RC =
             new Vector3f(0.0f, 0.498f, 0.0153f); // "vector render color" for square
